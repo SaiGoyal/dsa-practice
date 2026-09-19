85b0839281454b75d774ce0efde1344663844cc7
Sai Goyal <saigoyal78@gmail.com>
2026-09-19 19:22:28 +0530

    added a 2nd approach for majority element problem

--- arrays_and_hashing/majority_element.py
+++ arrays_and_hashing/majority_element.py
@@ -1,3 +1,4 @@
+#hashmap approach O(n) space and TC
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
         hashmap = {}
@@ -12,3 +13,16 @@ class Solution:
         return majority_element
 sol = Solution()
 print(sol.majorityElement([5,5,1,1,1,5,5]))
+
+#boyer moore majority voting algorithm O(1) space, O(N) TC
+# class Solution:
+#     def majorityElement(self, nums: list[int]) -> int:
+#         candidate = 0
+#         count = 0
+#         for num in nums:
+#             if count == 0:
+#                 candidate = num
+#             count += (1 if num == candidate else -1)
+#         return candidate
+# sol = Solution()
+# print(sol.majorityElement([5,5,1,1,1,5,5]))
